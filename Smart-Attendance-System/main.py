0342356fcf599600d9f2913444e7a366d757694a
gh-kg26 <guptakushagra202@gmail.com>
2023-05-17 14:55:28 +0530

    Feature update

--- Smart-Attendance-System/main.py
+++ Smart-Attendance-System/main.py
@@ -19,10 +19,10 @@ cap = cv2.VideoCapture(0)
 def detect_emotions(frame):
     # Load the pre-trained face detection classifier
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    
+
     # Load the pre-trained emotion detection model
     model = load_model('emotion_detection.h5')
-    
+
     # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -31,7 +31,7 @@ def detect_emotions(frame):
 
     # For each face detected, predict the emotion
     for (x, y, w, h) in faces:
-        face_roi = gray[y:y+h, x:x+w]
+        face_roi = gray[y:y + h, x:x + w]
         face_roi = cv2.resize(face_roi, (64, 64))
         face_roi = face_roi.astype('float') / 255.0
         face_roi = np.expand_dims(face_roi, axis=0)
@@ -42,10 +42,12 @@ def detect_emotions(frame):
         emotion = emotions[max_index]
         emotion_probability = emotion_predictions[max_index]
         # Draw a rectangle around the face and display the predicted emotion
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-        cv2.putText(frame, emotion + ' ({:.2f}%)'.format(emotion_probability*100), (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
-        
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(frame, emotion + ' ({:.2f}%)'.format(emotion_probability * 100), (x, y - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255, 12), 2)
+
     return frame
+
 
 video_capture = cv2.VideoCapture(0)
 
@@ -74,14 +76,14 @@ s = True
 now = datetime.now()
 current_date = now.strftime("%Y-%m-%d")
 
-f = open(current_date+'.csv','w+',newline='')
+f = open(current_date + '.csv', 'w+', newline='')
 lnwriter = csv.writer(f)
 
 while True:
     _, frame = video_capture.read()
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
     rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
-    
+
     if s:
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
@@ -95,7 +97,15 @@ while True:
         if matches[best_match_index]:
             name = known_faces_names[best_match_index]
 
-        face_names.append(name)
+        if name and name not in face_names:
+            face_names.append(name)
+
+        if name in students:
+            students.remove(name)
+            print(students)
+            current_time = now.strftime("%H-%M-%S")
+            lnwriter.writerow([name, current_time])
+
         if name in known_faces_names:
             font = cv2.ACCESS_MASK
             bottomLeftCornerOfText = (10, 100)
@@ -107,14 +117,9 @@ while True:
             thickness = 6
             lineType = 2
 
-            cv2.putText(frame, name+' Present', bottomLeftCornerOfText, font, fontScale, fontColor, thickness, lineType)
+            cv2.putText(frame, name + ' Present', bottomLeftCornerOfText, font, fontScale, fontColor, thickness,
+                        lineType)
 
-        if name in students:
-         students.remove(name)
-        print(students)
-        current_time = now.strftime("%H-%M-%S")
-        lnwriter.writerow([name, current_time])
-        
     # Check posture
     if len(face_locations) > 0:
         top, right, bottom, left = face_locations[0]
@@ -123,16 +128,16 @@ while True:
         aspect_ratio = x_diff / y_diff
         if aspect_ratio > 1.9 or aspect_ratio < 1:
             # Posture not straight
-            cv2.putText(frame, name+ " Please keep your posture straight!", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-            
+            cv2.putText(frame, name + " Please keep your posture straight!", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                        (0, 0, 255), 2)
+
     # Emotion detection
     frame = detect_emotions(frame)
 
-    
     cv2.imshow("Advanced Attendance system ©Kushagra & Vaishnavi 2023", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-
 video_capture.release()
-cv2.destroyAllWindows
+f.close()
+cv2.destroyAllWindows()
